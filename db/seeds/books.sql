4c0580aacc43880c5a11d2c96c57d9ec73aa1ff3
aliaksei babkou <albabkou@gmail.com>
2017-05-23 16:46:37 -0400

    created seeds

---
+++ db/seeds/books.sql
@@ -0,0 +1,29 @@
+INSERT INTO books (title, author, genre, isbn, description, rating, image_url) VALUES
+(
+  'Harry Potter and something',
+  'J.K. Rowling',
+  'fantasy',
+  345345,
+  'Something unclear about a kid in the glasses',
+  4,
+  'http://books.google.com/books/content?id=gK98gXR8onwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+),
+(
+  'Harry Potter and something else',
+  'J.K. Rowling',
+  'fantasy',
+  2344567,
+  'Something unclear about a kid in the glasses',
+  5,
+  'http://books.google.com/books/content?id=0ycuygAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+),
+(
+  'Harry Potter and a pile of something',
+  'J.K. Rowling',
+  'fantasy',
+  0789567,
+  'Something unclear about a kid in the glasses',
+  5,
+  'https://books.google.com/books/content?id=pIs9Em38dAoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+)
+
---
+++ db/seeds/users.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (username, email, password) VALUES
+(
+  'Joe Doe',
+  'joe@gmail.com',
+  '123456'
+),
+(
+  'Aliaksei Babkou',
+  'aliaksei@gmail.com',
+  '123456'
+),
+(
+  'Mishka',
+  'mishka@gmail.com',
+  '123456'
+)
