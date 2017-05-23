f6056a757ad969d4d5f3b7ddb0356095af06cc8d
lisa louison <lisa.louison@gmail.com>
2017-05-23 18:34:24 -0400

    updated default date in users_books table

--- db/migrations/migrations_05232017.sql
+++ db/migrations/migrations_05232017.sql
@@ -1,12 +1,15 @@
-\connect book_bunch_development   --connecting to our main database
+--connecting to our main database
+\connect book_bunch_development   
 
+-- making the username and email unique ensures that no two people can have the same username or email
 CREATE TABLE IF NOT EXISTS users (
   id BIGSERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL, --unique because no two or more people can have the same usernme
-  email VARCHAR(255) UNIQUE NOT NULL, --ensures that more than one person is not using the same email address
+  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL, 
   password VARCHAR(255) NOT NULL 
 );
 
+-- Looking online we found the best for our pupose was double precision becaus eour ratings are to one decimal point (example: rating:4.5)
 CREATE TABLE IF NOT EXISTS books (
   id BIGSERIAL PRIMARY KEY,
   title VARCHAR(1024),
@@ -14,17 +17,17 @@ CREATE TABLE IF NOT EXISTS books (
   genre VARCHAR(255),
   isbn INTEGER,
   description VARCHAR(1024),
-  rating DOUBLE PRECISION, --Looking online we found the best for our pupose was double precision becaus eour ratings are to one decimal point (example: rating:4.5)
+  rating DOUBLE PRECISION,
   image_url VARCHAR(1024)
 );
 
-
+-- this third table references the users table and the books table. The status, review, and dates columns have default values because they are added to the database separate from the edit page
 CREATE TABLE IF NOT EXISTS users_books (
   id BIGSERIAL PRIMARY KEY,
-  user_ref_id INTEGER REFERENCES users(id)--This references the id integer from the users table
-  book_ref_id INTEGER REFERENCES books(id), --This references the id integer from the books table
-  status  VARCHAR(255) DEFAULT 'To Read', --the default for this column is to read because it hasn't been read intitially 
+  user_ref_id INTEGER REFERENCES users(id),
+  book_ref_id INTEGER REFERENCES books(id), 
+  status  VARCHAR(255) DEFAULT 'To Read',  
   review VARCHAR(1024) DEFAULT '', 
-  date_started DATE DEFAULT '0000-00-00',
-  date_finished DATE DEFAULT '0000-00-00'
+  date_started DATE DEFAULT '1917-01-01',
+  date_finished DATE DEFAULT '1917-01-01'
 );
