f48022378ac6ce4d0c69c61036eb4f1b0570bb75
student <1abbasia@gmail.com>
2017-05-23 16:49:24 -0400

    updated migrations with default values

--- db/migrations/migrations_05232017.sql
+++ db/migrations/migrations_05232017.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE IF NOT EXISTS users ( --This is our users database with the columns of id, username,email, and password
   id BIGSERIAL PRIMARY KEY,
-  username VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255)
+  username VARCHAR(255) UNIQUE NOT NULL, --unique because no two or mor people can have the same usernme
+  email VARCHAR(255) UNIQUE NOT NULL, --ensures that more than one person is not using the saem email adress
+  password VARCHAR(255) NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS books ( --This is our books database with the columns of id, title, etc... 
@@ -24,8 +24,8 @@ CREATE TABLE IF NOT EXISTS users_books ( --user books table.
   id BIGSERIAL PRIMARY KEY,
   user_ref_id INTEGER REFERENCES users(id)--This refernces the id integer from the users table. 
   book_ref_id INTEGER,
-  status  VARCHAR(255),
-  review VARCHAR(1024),
-  date_started DATE,
+  status  VARCHAR(255) DEFAULT 'To Read', --the default for this column is to read because it hasnt been read intitially 
+  review VARCHAR(1024) DEFAULT NOT NULL, 
+  date_started DATE DEFAULT '0000-00-00',--set a default date
   date_finished DATE
 );
