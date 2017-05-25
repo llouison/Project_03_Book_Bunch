1d521fa5544a70f9e9388b09f40764fe98ff6d7e
lisa louison <lisa.louison@gmail.com>
2017-05-25 12:55:33 -0400

    added auth helpers to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// setting up express and importing helper node modules
+/* setting up express and importing helper node modules*/
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -8,38 +8,49 @@ const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
-// creating a variable for the express function
+/* dotenv locats the environment variables in the .env file */
+require('dotenv').config();
+/* creating a variable for the express function*/
 const app = express();
 
-// setting up port to listen to
+/* setting up port to listen to*/
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
 });
 
-// setting up static file redirect
+/* setting up static file redirect*/
 app.use('/static', express.static(path.join(__dirname, 'public')));
-// setting up cors
+/* setting up cors*/
 app.use(cors());
 /* setting up logger to log activity when the server is running */
 app.use(logger('dev'));
-// setting up body parser to parse the request object
+app.use(cookieParser());
+/* setting up body parser to parse the request object*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* ###### setting routes ###### */
-// index route
+/* index route*/
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// importing routes and assigning urls for books api and users api
+/* importing routes and assigning urls for books api and users api*/
 const booksRoutes = require('./routes/booksRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 app.use('/api/books', booksRoutes);
 app.use('/api/users', usersRoutes);
 
-// handling 404
+/* handling 404*/
 app.get('*', function(req,res) {
     res.status(404).send({message: 'Oops! Not found.'});
 });
