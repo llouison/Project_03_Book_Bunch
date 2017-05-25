3f5485bded4bf657d855c3cbd1f680fe4a0988dc
lisa louison <lisa.louison@gmail.com>
2017-05-25 10:17:08 -0400

    imported react router and user components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/partials/Header';
+import UserDash from './components/UserDash';
+import UserBook from './components/UserBook';
 import Footer from './components/partials/Footer';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import './App.css';
 
 class App extends Component {
@@ -29,10 +38,18 @@ componentDidMount(){
 }
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Footer />
-      </div>
+       <Router>
+        <div className="app">
+          <main>
+            <Header />
+            {/*<Route exact path="/" component={Index} />
+            <Route path="/search" component={Search} />*/}
+            <Route path="/user/:id" component={UserDash} />
+            <Route path="/user/:id/:isbn" component={UserBook} />
+            <Footer />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
