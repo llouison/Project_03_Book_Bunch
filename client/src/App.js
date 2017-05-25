3a02da6d16027bfbf9070cd68f38cba1331bd6b9
aliaksei babkou <albabkou@gmail.com>
2017-05-25 12:38:13 -0400

    updated App.js route for registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/partials/Header';
+import Index from './components/Index';
+import RegistrationForm from './components/RegistrationForm';
 import UserDash from './components/UserDash';
 import UserBook from './components/UserBook';
 import Footer from './components/partials/Footer';
@@ -42,10 +44,11 @@ componentDidMount(){
         <div className="app">
           <main>
             <Header />
-            {/*<Route exact path="/" component={Index} />
-            <Route path="/search" component={Search} />*/}
+            <Route exact path="/" component={Index} />
+            {/*<Route path="/search" component={Search} />*/}
             <Route path="/user/:id" component={UserDash} />
             <Route path="/user/:id/:isbn" component={UserBook} />
+            <Route path="/register" component={RegistrationForm} />
             <Footer />
           </main>
         </div>
