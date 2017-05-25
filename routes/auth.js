ed1e6434d63133d6e05cbb2bc5b19c16532058f0
lisa louison <lisa.louison@gmail.com>
2017-05-25 12:21:05 -0400

    created authentication routes

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+//importing express and user controller
+const express = require('express');
+const controller = require('../controllers/usersController');
+// creating a variable for the express router method
+const router = express.Router();
+// importing the authHelpers middleware
+const authHelpers = require('../services/auth/authHelpers');
+const passport = require('../services/auth/local');
+
+router.get('/login', (req, res) => {
+  res.render('auth/log-in', {
+    documentTitle: 'Adaquotes login',
+  });
+});
+router.get('/register', (req, res) => {
+  res.render('auth/register', {
+    documentTitle: 'Adaquotes registration',
+  });
+});
+router.post('/register', controller.create);
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: false,
+  })
+);
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
