48836c691c30ef15e83676ed05a5526a70abb12e
lisa louison <lisa.louison@gmail.com>
2017-05-25 15:07:51 -0400

    changed res.render to res.json in auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -8,14 +8,11 @@ const authHelpers = require('../services/auth/authHelpers');
 const passport = require('../services/auth/local');
 
 router.get('/login', (req, res) => {
-  res.render('auth/log-in', {
-    documentTitle: 'Adaquotes login',
-  });
+    console.log(res);
+//   res.json(res);
 });
 router.get('/register', (req, res) => {
-  res.render('auth/register', {
-    documentTitle: 'Adaquotes registration',
-  });
+  res.json(res);
 });
 router.post('/register', controller.create);
 router.post(
