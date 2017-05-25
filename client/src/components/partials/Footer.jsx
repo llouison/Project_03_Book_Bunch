d18a040e8b576a0d217afa7b30bfaa31dcf70559
lisa louison <lisa.louison@gmail.com>
2017-05-25 09:45:22 -0400

    created footer component

---
+++ client/src/components/partials/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>Created by <a href='https://github.com/llouison/Project_03_Book_Bunch'>Team A.L.A</a>
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
