ab25995dcca3bcd67067e7707be93c97bdc3e4e5
student <1abbasia@gmail.com>
2017-05-25 15:04:00 -0400

    work so far for search componenet

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [],
+      books: [], //Using these values when I'm calling them in /src/components/SearchBookForm as the props
+      inputTitletValue:'',
+      inputAuthorValue:'',
+      inputIsbnValue:'',
+      inputGenreValue:'',
+
+
     }
 
   }
 
 componentDidMount(){
-  fetch('/api/books')
+  fetch('https://www.googleapis.com/books/v1/volumes?q=inauthor:rowling+intitle:chamber&key=AIzaSyBSbTuoPrwQ0PvCFj0uhq2MtGh3MEaoW0Y')
   .then((response) => {
     return response.json()
   })
@@ -21,11 +26,53 @@ componentDidMount(){
     //setting the state//
     this.setState((prevState) => {
           return {
-            books: responseJson.data.books, //from api
+            books: responseJson, //from api
           }
          });
+      // console.log(responseJson.data.books)
   });
 }
+
+handleInputTitleChange(event) {
+    this.setState({inputTitleValue: event.target.value});
+  }
+
+handleInputAuthorChange(event) {
+    this.setState({inputAuthorValue: event.target.value});
+  }
+
+handleInputIsbnChange(event) {
+    this.setState({inputIsbnValue: event.target.value});
+  }
+
+handleInputGenreChange(event) {
+    this.setState({inputAuthorValue: event.target.value});
+  }
+
+// handleFormSubmit(event) {
+//     fetch('https://', { //Does the api we make go here or the NYTimes api?
+//       method: 'POST',
+//       headers: {'Content-Type': 'application/json'},
+//       body: JSON.stringify({
+//         title: event.target.title.value,
+//         author: event.target.author.value,
+//         isbn: event.target.isbn.value,
+//         genre: event.target.genre.value
+//       }),
+//     })
+//     .then((response) => {
+//       return response.json()
+//     })
+    
+//         this.setState((prevState) => {
+//           return {
+//             quotes: prevState.quotes.concat(newQuote),
+//             inputContentValue: '',
+//             inputAuthorValue: '',
+//             inputGenreValue: '',
+//           }
+//         })
+//       } 
   render() {
     return (
       <div className="App">
