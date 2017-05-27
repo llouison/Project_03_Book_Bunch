a9da2de5d59b2848fcea164093215d4e798790ce
lisa louison <lisa.louison@gmail.com>
2017-05-27 14:09:50 -0400

    updated search result display properties

--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchResult extends Component {
+  addUserBook(event){
+    event.preventDefault();
+    console.log('add it!');
+  }
+
   render() {
     return(
       <li className='book_result'>
         <p>Title:{this.props.volumeInfo.title}</p>
         <p>Author: {this.props.volumeInfo.authors}</p>
+        <p>Genre: {this.props.volumeInfo.categories}</p>
+        <p>ISBN: {this.props.volumeInfo.industryIdentifiers[0].identifier}</p>
         <p>Description: {this.props.volumeInfo.description}</p>
-        <button>Add It</button>
+        <p>Rating: {this.props.volumeInfo.averageRating}</p>
+        <p>Image URL: <a href='{this.props.volumeInfo.imageLinks.thumbnail}' target='_blank'>{this.props.volumeInfo.imageLinks.thumbnail}</a></p>
+        <form
+        className="add_book_form"
+        onSubmit={this.addUserBook}
+        >
+          <button>Add It</button>
+        </form>
       </li>
     );
   }
