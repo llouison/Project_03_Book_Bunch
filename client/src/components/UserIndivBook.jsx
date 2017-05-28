54c834e9ff3e8c0946c3fafa8d7ea524383060d9
lisa louison <lisa.louison@gmail.com>
2017-05-28 12:24:15 -0400

    displaying edit userinfo based on isbeing edited status

--- client/src/components/UserIndivBook.jsx
+++ client/src/components/UserIndivBook.jsx
@@ -7,10 +7,22 @@ class UserBook extends Component {
         this.state = {
         user: this.props.user,
         userId: this.props.userId,
+        review: '',
         usersBook: [],
+        status: '',
+        date_started: null,
+        date_finished: null,
+        isBeingEdited: false,
         }
         this.getIndivBook = this.getIndivBook.bind(this);
         this.updateUsersBook = this.updateUsersBook.bind(this);
+        this.displayUserInfo = this.displayUserInfo.bind(this);
+        this.renderUserInfo = this.renderUserInfo.bind(this);
+        this.handleReviewChange = this.handleReviewChange.bind(this);
+        this.handleStatusChange = this.handleStatusChange.bind(this);
+        this.handleDateStartedChange = this.handleDateStartedChange.bind(this);
+        this.handleDateFinishedChange = this.handleDateFinishedChange.bind(this);
+        this.handleUserUpdate = this.handleUserUpdate.bind(this);
     }
 
     componentDidMount(){
@@ -31,28 +43,114 @@ class UserBook extends Component {
         this.setState((prevState) => {
             return {
                 usersBook: book,
+                review: book.review,
+                status: book.status,
+                date_started: book.date_started,
+                date_finished: book.date_finished,
             }
         })
     }
 
-  render() {
-    return (
-      <div>
-        <Header path1='/' link1='Home' path2='/search' link2='Search' path3='/logout' link3='Logout'/>
-        <img src={this.state.usersBook.image_url} alt={this.state.usersBook.title}/>
-        <p>Title: {this.state.usersBook.title}</p>
-        <p>Author: {this.state.usersBook.author}</p>
-        <p>Genre: {this.state.usersBook.genre}</p>
-        <p>Rating: {this.state.usersBook.rating}</p>
-        <p>ISBN: {this.props.match.params.isbn}</p>
-        <p>Description: {this.state.usersBook.description}</p>
-        <p>Review: {this.state.usersBook.review}</p>
-        <p>Status: {this.state.usersBook.status}</p>
-        <p>Date Started: {this.state.usersBook.date_started}</p>
-        <p>Date Finished: {this.state.usersBook.date_finished}</p>
-      </div>
-    );
-  }
+    displayUserInfo(){
+        if (this.state.isBeingEdited === false) {
+            return this.renderUserInfo();
+        } else {
+            return this.renderEditForm();
+        }
+    }
+
+    renderUserInfo(){
+        return(
+            <div>
+                <p>Review: {this.state.review}</p>
+                <p>Status: {this.state.status}</p>
+                <p>Date Started: {this.state.date_started}</p>
+                <p>Date Finished: {this.state.date_finished}</p>
+                <button onClick={() => {this.setState({isBeingEdited: true}) }}>Edit</button>
+            </div>
+        )
+    }
+
+    renderEditForm(){
+        return (
+            <div>
+                <form
+                    onSubmit={(event) => {
+                        this.props.handleUserUpdate(event);
+                        this.setState({isBeingEdited: false});
+                    }}
+                >
+                    <label/>Review:<textarea
+                        type="text"
+                        value={this.state.review}
+                        name='review'
+                        onChange={this.handleReviewChange}>
+                    </textarea><br/>
+                    <label/>Status:<select name='status' value={this.state.status} onChange={this.handleStatusChange}>
+                        <option value="Reading">Reading</option>
+                        <option value="Read">Read</option>
+                        <option selected value="To Read">To Read</option>
+                    </select><br/>
+                    <label/>Date Started:<input
+                        type="date"
+                        value={this.state.date_started}
+                        name='date_started'
+                        onChange={this.handleDateStartedChange}
+                    /><br/>
+                    <label/>Date Finished:<input
+                        type="date"
+                        value={this.state.date_finished}
+                        name='date_finished'
+                        onChange={this.handleDateFinishedChange}
+                    /><br/>
+                    <button>Update</button>
+                </form>
+                <button>Delete</button>
+            </div>
+        )
+    }
+
+    handleReviewChange(event){
+        console.log('change');
+        this.setState({review: event.target.value});
+    }
+
+    handleStatusChange(event){
+        console.log('change');
+        this.setState({status: event.target.value});
+    }
+
+    handleDateStartedChange(event){
+        console.log('change');
+        this.setState({date_started: event.target.value});
+    }
+
+    handleDateFinishedChange(event){
+        console.log('change');
+        this.setState({date_started: event.target.value});
+    }
+
+    handleUserUpdate(event){
+        event.preventDefault();
+        console.log('update it!');
+    }
+
+
+    render() {
+        return (
+        <div>
+            <Header path1='/' link1='Home' path2='/search' link2='Search' path3='/logout' link3='Logout'/>
+            <img src={this.state.usersBook.image_url} alt={this.state.usersBook.title}/>
+            <p>Title: {this.state.usersBook.title}</p>
+            <p>Author: {this.state.usersBook.author}</p>
+            <p>Genre: {this.state.usersBook.genre}</p>
+            <p>Rating: {this.state.usersBook.rating}</p>
+            <p>ISBN: {this.props.match.params.isbn}</p>
+            <p>Description: {this.state.usersBook.description}</p>
+            {this.displayUserInfo()}
+        </div>
+        );
+    }
 }
 
 export default UserBook;
