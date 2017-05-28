96e63f19b29cb84ea2a43198c9cb4fb7e0d935d9
lisa louison <lisa.louison@gmail.com>
2017-05-27 23:40:19 -0400

    updated book component

---
+++ client/src/components/Book.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class UserBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: `/user/${this.props.book.isbn}`,
+    }
+  }
+  render() {
+    return (
+      <div>
+        <a href={this.state.path}>
+          <img src={this.props.book.image_url} alt={this.props.book.title}/> 
+        </a>
+        <p>title: {this.props.book.title}</p>
+      </div>
+    );
+  }
+}
+
+export default UserBook;
