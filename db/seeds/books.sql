00b63b524327e9afd2f62b12613888ee3d9026c0
lisa louison <lisa.louison@gmail.com>
2017-05-27 23:41:32 -0400

    updated book info

--- db/seeds/books.sql
+++ db/seeds/books.sql
@@ -1,29 +1,29 @@
 INSERT INTO books (title, author, genre, isbn, description, rating, image_url) VALUES
 (
-  'Harry Potter and something',
+  'Harry Potter and the Sorcerer''s Stone',
   'J.K. Rowling',
-  'fantasy',
-  345345,
-  'Something unclear about a kid in the glasses',
-  4,
-  'http://books.google.com/books/content?id=gK98gXR8onwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  'Juvenile Fiction',
+  1781100489,
+  'Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on his eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin!',
+  4.5,
+  'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
 ),
 (
-  'Harry Potter and something else',
-  'J.K. Rowling',
-  'fantasy',
-  2344567,
-  'Something unclear about a kid in the glasses',
-  5,
-  'http://books.google.com/books/content?id=0ycuygAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  'Why Didn''t They Teach Me This in School?',
+  'Cary Siegel',
+  'Business & Economics',
+  1481027565,
+  'Why do high schools and colleges require students to take courses in English, math and science, yet have absolutely no requirements for students to learn about personal money management? Why Didn''t They Teach Me This in School? 99 Personal Money Management Lessons to Live By was initially developed by the author to pass on to his five children as they entered adulthood. The book includes eight important lessons focusing on 99 principles that will quickly and memorably enhance any individual''s money management acumen.',
+  null,
+  'http://books.google.com/books/content?id=jUd9mQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
 ),
 (
-  'Harry Potter and a pile of something',
-  'J.K. Rowling',
-  'fantasy',
-  0789567,
-  'Something unclear about a kid in the glasses',
-  5,
-  'https://books.google.com/books/content?id=pIs9Em38dAoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  'Outliers',
+  'Malcolm Gladwell',
+  'Psychology',
+  0316040347,
+  'There is a story that is usually told about extremely successful people, a story that focuses on intelligence and ambition. Gladwell argues that the true story of success is very different, and that if we want to understand how some people thrive, we should spend more time looking around them-at such things as their family, their birthplace, or even their birth date. And in revealing that hidden logic, Gladwell presents a fascinating and provocative blueprint for making the most of human potential. In The Tipping Point Gladwell changed the way we understand the world. In Blink he changed the way we think about thinking. In OUTLIERS he transforms the way we understand success.',
+  3.5,
+  'http://books.google.com/books/content?id=3NSImqqnxnkC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
 );
 
