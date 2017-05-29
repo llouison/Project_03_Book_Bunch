54deaf5d52f6747effeb8b410fa33f13e94df8af
lisa louison <lisa.louison@gmail.com>
2017-05-28 23:10:33 -0400

    added state and get usersbooks functions

--- client/src/components/UserDash.jsx
+++ client/src/components/UserDash.jsx
@@ -5,8 +5,35 @@ import Header from './partials/Header';
 class UserDash extends Component {
  constructor(props) {
     super(props);
+    this.state = {
+      usersBooks: [],
+    }
+    this.getUsersBooks = this.getUsersBooks.bind(this);
+    this.updateUsersBooks = this.updateUsersBooks.bind(this);
     this.emptyList = this.emptyList.bind(this);
     this.renderBooks = this.renderBooks.bind(this);
+  }
+
+  componentDidMount(){
+    this.getUsersBooks()
+  }
+
+  getUsersBooks(){
+    fetch(`/api/users/${this.props.userId}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((responseJson) => {
+        this.updateUsersBooks(responseJson.data.usersBooks);
+    });
+  }
+
+   updateUsersBooks(books){
+     this.setState((prevState) => {
+      return {
+        usersBooks: books,
+      }
+	  })
   }
 
   emptyList(){
@@ -24,9 +51,20 @@ class UserDash extends Component {
       <div>
         <Header path1='/' link1='Home' path2='/search' link2='Search' path3='/logout' link3='Logout'/>
         <p>Welcome back, {this.props.user}</p>
-        <p>There are {this.props.usersBooks.length} books in your collection</p>
-        {this.props.usersBooks.map((book) => {
-          if (book.status === 'To Read'){
+        <p>There are {this.state.usersBooks.length} books in your collection</p>
+        {this.state.usersBooks.map((book) => {
+          if (book.status === "To Read"){
+          return (
+            <div className='shelf' key={book.isbn}>
+              <h2>To Read</h2>
+              <Book 
+                user={this.props.user} 
+                book={book}
+              />
+            </div>
+            )
+          }
+          else if (book.status === "Reading"){
           return (
             <div className='shelf' key={book.isbn}>
               <h2>Reading</h2>
@@ -37,21 +75,10 @@ class UserDash extends Component {
             </div>
             )
           }
-          if (book.status === 'Reading'){
+          else if (book.status === "Read"){
           return (
             <div className='shelf' key={book.isbn}>
               <h2>Read</h2>
-              <Book 
-                user={this.props.user} 
-                book={book}
-              />
-            </div>
-            )
-          }
-          if (book.status === 'Read'){
-          return (
-            <div className='shelf' key={book.isbn}>
-              <h2>To Read</h2>
               <Book 
                 user={this.props.user} 
                 book={book}
@@ -65,8 +92,7 @@ class UserDash extends Component {
   }
   
   render() {
-    // console.log('user dash', this.props.usersBooks.length)
-    if (this.props.usersBooks.length === 0) {
+    if (this.state.usersBooks.length === 0) {
       return this.emptyList();
     } else {
       return this.renderBooks();
