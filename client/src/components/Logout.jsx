728d41eb385107d9c2d672e1b9d0c1f49adeb26b
lisa louison <lisa.louison@gmail.com>
2017-05-30 00:35:35 -0400

    styling logout page

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -5,15 +5,20 @@ import Footer from './partials/Footer';
 class Login extends Component {
   render() {
     return (
-      <div>
+      <div className='page'>
         <Header path1='/' link1='Home' path2='/user' link2='My Collection' path3='/search' link3='Search' />
+        <div className='main_container'>
         <h2>Thank you for using Book Bunch {this.props.user}</h2>
+        <div className='form_container'>
         <form
           className="logout_form"
           onSubmit={this.props.handleLogoutSubmit}
         >
           <button>Logout</button>
         </form>
+        </div>
+        {/*<img src={require('../images/bookstack.png')}/>*/}
+        </div>
         <Footer />
       </div>
     );
